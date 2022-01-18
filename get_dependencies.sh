4d748686be8ef688d106b78d85a984470e094e83
Bao <dowrite4@gmail.com>
2022-01-18 22:34:36 +0000

    Changed "local internet" to be hosted on S3

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,4 +1,10 @@
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
+apt install ansible
+apt install vagrant
 ansible-galaxy collection install community.windows
+ansible-galaxy collection install community.general
+vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-scp
+
