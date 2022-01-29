bee99c175a771927d0bc8790697168f18a793a6c
Kali <kali@kali>
2022-01-29 18:17:57 -0500

    Updated dependencies

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,8 +1,12 @@
-virtualenv -p python3 venv
+#!/bin/bash
+sudo apt update 
+sudo apt install -y python3-dev linux-headers-generic python3-pip python3-virtualenv
+sudo apt install -y ansible vagrant virtualbox virtualbox-dkms unzip git
+sudo gem install winrm-elevated
+sudo gem install winrm
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-apt install ansible
-apt install vagrant
 ansible-galaxy collection install community.windows
 ansible-galaxy collection install community.general
 vagrant plugin install vagrant-vbguest
